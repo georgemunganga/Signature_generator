d0f879466c5605e0c60ac46c6f15435a986137d2
Mighty Finance <mightyfin@users.noreply.github.com>
2026-07-08 11:18:55 +0000

    feat: use company logo as favicon

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,36 @@
 import { Toaster } from 'vue-sonner'
 import 'vue-sonner/style.css'
 
+import { useBranding } from '@/composables/useBranding'
+
 const { init } = useSignatures()
+const { companyName, logoUrl } = useBranding()
+
+function updateFavicon(href: string) {
+  let favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]')
+
+  if (!favicon) {
+    favicon = document.createElement('link')
+    favicon.rel = 'icon'
+    document.head.appendChild(favicon)
+  }
+
+  favicon.href = href || '/favicon.ico'
+}
+
+function updateDocumentBranding() {
+  updateFavicon(logoUrl.value)
+  document.title = `${companyName.value || 'Company'} E-Signatures`
+}
 
 onMounted(() => {
   document.documentElement.classList.remove('dark')
   localStorage.setItem('vueuse-color-scheme', 'light')
   init()
+  updateDocumentBranding()
 })
+
+watch([logoUrl, companyName], updateDocumentBranding)
 </script>
 
 <template>
